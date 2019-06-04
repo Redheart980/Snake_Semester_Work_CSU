8b413d98f31db3b2248ce6176d1bd77853fd6f68
Redheart <battlefield980@mail.ru>
2019-06-04 20:33:40 +0500

    Добавьте файлы проекта.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Snake
+{
+    partial class Form_Snake
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Complexity = new System.Windows.Forms.TrackBar();
+            this.Complexity_TextBox = new System.Windows.Forms.TextBox();
+            this.ScoreText_TextBox = new System.Windows.Forms.TextBox();
+            this.Score_TextBox = new System.Windows.Forms.TextBox();
+            this.Start_button = new System.Windows.Forms.Button();
+            this.Stop_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Complexity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_tick);
+            // 
+            // Complexity
+            // 
+            this.Complexity.Location = new System.Drawing.Point(600, 100);
+            this.Complexity.Margin = new System.Windows.Forms.Padding(0);
+            this.Complexity.Maximum = 1000;
+            this.Complexity.Minimum = 100;
+            this.Complexity.Name = "Complexity";
+            this.Complexity.Size = new System.Drawing.Size(172, 45);
+            this.Complexity.SmallChange = 10;
+            this.Complexity.TabIndex = 0;
+            this.Complexity.TabStop = false;
+            this.Complexity.TickFrequency = 20;
+            this.Complexity.Value = 100;
+            this.Complexity.ValueChanged += new System.EventHandler(this.Complexity_ValueChanged);
+            // 
+            // Complexity_TextBox
+            // 
+            this.Complexity_TextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.Complexity_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Complexity_TextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Complexity_TextBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Complexity_TextBox.Location = new System.Drawing.Point(634, 74);
+            this.Complexity_TextBox.Name = "Complexity_TextBox";
+            this.Complexity_TextBox.ReadOnly = true;
+            this.Complexity_TextBox.Size = new System.Drawing.Size(100, 13);
+            this.Complexity_TextBox.TabIndex = 1;
+            this.Complexity_TextBox.TabStop = false;
+            this.Complexity_TextBox.Text = "Сложность";
+            this.Complexity_TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ScoreText_TextBox
+            // 
+            this.ScoreText_TextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.ScoreText_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ScoreText_TextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ScoreText_TextBox.Location = new System.Drawing.Point(600, 148);
+            this.ScoreText_TextBox.Name = "ScoreText_TextBox";
+            this.ScoreText_TextBox.ReadOnly = true;
+            this.ScoreText_TextBox.Size = new System.Drawing.Size(100, 13);
+            this.ScoreText_TextBox.TabIndex = 2;
+            this.ScoreText_TextBox.TabStop = false;
+            this.ScoreText_TextBox.Text = "Очки:";
+            // 
+            // Score_TextBox
+            // 
+            this.Score_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Score_TextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Score_TextBox.ForeColor = System.Drawing.Color.Black;
+            this.Score_TextBox.Location = new System.Drawing.Point(600, 167);
+            this.Score_TextBox.Name = "Score_TextBox";
+            this.Score_TextBox.ReadOnly = true;
+            this.Score_TextBox.Size = new System.Drawing.Size(100, 13);
+            this.Score_TextBox.TabIndex = 3;
+            this.Score_TextBox.TabStop = false;
+            this.Score_TextBox.Text = "0";
+            // 
+            // Start_button
+            // 
+            this.Start_button.Location = new System.Drawing.Point(600, 214);
+            this.Start_button.Name = "Start_button";
+            this.Start_button.Size = new System.Drawing.Size(75, 23);
+            this.Start_button.TabIndex = 4;
+            this.Start_button.Text = "Начать";
+            this.Start_button.UseVisualStyleBackColor = true;
+            this.Start_button.Click += new System.EventHandler(this.Start_button_Click);
+            // 
+            // Stop_button
+            // 
+            this.Stop_button.Enabled = false;
+            this.Stop_button.Location = new System.Drawing.Point(600, 243);
+            this.Stop_button.Name = "Stop_button";
+            this.Stop_button.Size = new System.Drawing.Size(75, 23);
+            this.Stop_button.TabIndex = 5;
+            this.Stop_button.Text = "Стоп";
+            this.Stop_button.UseVisualStyleBackColor = true;
+            this.Stop_button.Click += new System.EventHandler(this.Stop_button_Click);
+            // 
+            // Form_Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Stop_button);
+            this.Controls.Add(this.Start_button);
+            this.Controls.Add(this.Score_TextBox);
+            this.Controls.Add(this.ScoreText_TextBox);
+            this.Controls.Add(this.Complexity_TextBox);
+            this.Controls.Add(this.Complexity);
+            this.KeyPreview = true;
+            this.Name = "Form_Snake";
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Control);
+            ((System.ComponentModel.ISupportInitialize)(this.Complexity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TrackBar Complexity;
+        private System.Windows.Forms.TextBox Complexity_TextBox;
+        private System.Windows.Forms.TextBox ScoreText_TextBox;
+        private System.Windows.Forms.TextBox Score_TextBox;
+        private System.Windows.Forms.Button Start_button;
+        private System.Windows.Forms.Button Stop_button;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form_Snake : Form
+    {
+        Rectangle border = new Rectangle(50, 50, 500, 500);
+        MySnake gameSnake = new MySnake();
+        PictureBox Food = new PictureBox();
+        LogicMap gameMap = new LogicMap();
+        int factorScore = 100;
+        long Score = 0;
+        int nextX = 0;
+        int nextY = 0;
+        int pictureSize = 50;
+        Keys CurrentMove = Keys.Space;
+
+        public Form_Snake()
+        {
+            InitializeComponent();
+            Food.BackColor = Color.Yellow;
+            Food.Location = new Point(300, 300);
+            Food.Size = new Size(pictureSize, pictureSize);
+            Controls.Add(gameSnake.tail[0]);
+            Controls.Add(Food);
+            timer.Interval = 1000;
+            timer.Enabled = false;
+            KeyDown += new KeyEventHandler(Control);
+        }
+
+        public class MySnake
+        {
+            public PictureBox[] tail;
+            public int size;
+
+            public MySnake()
+            {
+                tail = new PictureBox[100];
+                tail[0] = new PictureBox();
+                tail[0].BackColor = Color.Green;
+                tail[0].Location = new Point(250, 250);
+                tail[0].Size = new Size(50, 50);
+                for (int i = 1; i < 100; i++)
+                    tail[i] = new PictureBox();
+                size = 1;
+            }
+        }
+
+        public class LogicMap //Примечание: 0 - пусто, 1 - змейка, 2 - еда
+        {
+            public int[,] map;
+            
+            public LogicMap()
+            {
+                map = new int[10, 10];
+                for (int x = 0; x < 10; x++)
+                    for (int y = 0; y < 10; y++)
+                        map[x, y] = 0;
+                map[4, 4] = 1;
+                map[5, 5] = 2;
+            }
+        }
+
+        private int LogicConv (int point) //Примечание: Преобразует координату в точку на логической карте
+        {
+            return point / pictureSize - 1;
+        }
+
+        private int LogicConvInv (int point) //Примечание: Преобразует точку на логической карте в координату
+        {
+            return (point + 1) * pictureSize;
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            Activate();
+            SneakMove();
+            EatFood();
+        }
+
+        private void SneakMove()
+        {
+            gameMap.map[LogicConv(gameSnake.tail[gameSnake.size - 1].Location.X), LogicConv(gameSnake.tail[gameSnake.size - 1].Location.Y)] = 0;
+            for (int i = gameSnake.size - 1; i > 0; i--)
+            {
+                gameSnake.tail[i].Location = gameSnake.tail[i - 1].Location;
+            }
+            gameSnake.tail[0].Location = new Point(gameSnake.tail[0].Location.X + nextX * 50, gameSnake.tail[0].Location.Y + nextY * 50);
+            if (CheckBorder())
+            {
+                if (gameMap.map[LogicConv(gameSnake.tail[0].Location.X), LogicConv(gameSnake.tail[0].Location.Y)] != 1)
+                    gameMap.map[LogicConv(gameSnake.tail[0].Location.X), LogicConv(gameSnake.tail[0].Location.Y)] = 1;
+                else
+                {
+                    timer.Enabled = false;
+                    MessageBox.Show("Вы укусили самого себя!\nВы набрали " + Score + " очков!", "Игра окончена", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    Score = 0;
+                    Score_TextBox.Text = "0";
+                }
+            }
+        }
+
+        private bool CheckBorder()
+        {
+            if (gameSnake.tail[0].Location.X < 50 || gameSnake.tail[0].Location.X > 500 || gameSnake.tail[0].Location.Y < 50 || gameSnake.tail[0].Location.Y > 500)
+            {
+                timer.Enabled = false;
+                MessageBox.Show("Вы вышли из своей зоны обитания!\nВы набрали " + Score + " очков!", "Игра окончена", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                Score = 0;
+                Score_TextBox.Text = "0";
+                return false;
+            }
+            return true;
+        }
+
+        private void EatFood()
+        {
+            if (gameSnake.tail[0].Location == Food.Location)
+            {
+                gameSnake.tail[gameSnake.size] = new PictureBox();
+                gameSnake.tail[gameSnake.size].Size = new Size(pictureSize, pictureSize);
+                gameSnake.tail[gameSnake.size].Location = gameSnake.tail[gameSnake.size - 1].Location;
+                gameSnake.tail[gameSnake.size].BackColor = Color.Orange;
+                Controls.Add(gameSnake.tail[gameSnake.size]);
+                gameSnake.size++;
+                Score += factorScore;
+                Score_TextBox.Text = Convert.ToString(Score);
+                if (gameSnake.size < 100)
+                    RestoreFood();
+                else
+                {
+                    timer.Enabled = false;
+                    MessageBox.Show("Вам больше некуда расти!\nВы набрали " + Score + " очков!", "Игра окончена", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    Score = 0;
+                    Score_TextBox.Text = "0";
+                }
+            }
+        }
+
+        private void RestoreFood()
+        {
+            Random rnd = new Random();
+            bool foodAdd = false;
+            int tryCount = 0;
+            int x;
+            int y;
+            while (!foodAdd || tryCount > 5)
+            {
+                x = rnd.Next(0, 9);
+                y = rnd.Next(0, 9);
+                if (gameMap.map[x,y] != 1)
+                {
+                    Food.Location = new Point(LogicConvInv(x), LogicConvInv(y));
+                    gameMap.map[x, y] = 2;
+                    foodAdd = true;
+                }
+                tryCount++;
+            }
+
+            if (!foodAdd)
+            {
+                switch(rnd.Next(0,1))
+                {
+                    case 0:
+                        for (x = 0; x < 10; x++)
+                        {
+                            for (y = 0; y < 10; y++)
+                            {
+                                if (gameMap.map[x,y] != 1)
+                                {
+                                    Food.Location = new Point(LogicConvInv(x), LogicConvInv(y));
+                                    gameMap.map[x, y] = 2;
+                                    foodAdd = true;
+                                    break;
+                                }
+                            }
+                            if (foodAdd)
+                                break;
+                        }
+                        break;
+                    case 1:
+                        for (x = 9; x >= 0; x--)
+                        {
+                            for (y = 0; y < 10; y++)
+                            {
+                                if (gameMap.map[x, y] != 1)
+                                {
+                                    Food.Location = new Point(LogicConvInv(x), LogicConvInv(y));
+                                    gameMap.map[x, y] = 2;
+                                    foodAdd = true;
+                                    break;
+                                }
+                            }
+                            if (foodAdd)
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Pen blackPen = new Pen(Color.Black, 1);
+            e.Graphics.DrawRectangle(blackPen, border);
+        }
+
+        private void Control(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:
+                    if (CurrentMove != Keys.Up)
+                    {
+                        nextX = 0;
+                        nextY = -1;
+                        CurrentMove = Keys.Up;
+                    }
+                    break;
+                case Keys.Down:
+                    if (CurrentMove != Keys.Up)
+                    {
+                        nextX = 0;
+                        nextY = 1;
+                        CurrentMove = Keys.Down;
+                    }
+                    break;
+                case Keys.Left:
+                    if (CurrentMove != Keys.Right)
+                    {
+                        nextX = -1;
+                        nextY = 0;
+                        CurrentMove = Keys.Left;
+                    }
+                    break;
+                case Keys.Right:
+                    if (CurrentMove != Keys.Left)
+                    {
+                        nextX = 1;
+                        nextY = 0;
+                        CurrentMove = Keys.Right;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Complexity_ValueChanged(object sender, EventArgs e)
+        {
+            timer.Interval = 1100 - Complexity.Value;
+            factorScore = Complexity.Value;
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            nextX = 0;
+            nextY = 0;
+            gameMap = new LogicMap();
+            Food.Location = new Point(300, 300);
+            gameSnake.size = 1;
+            gameSnake.tail[0].Location = new Point(250, 250);
+            for (int i = 1; i < 100; i++)
+                gameSnake.tail[i].Location = new Point(1000, 1000);
+            Complexity.Enabled = false;
+            CurrentMove = Keys.Space;
+            this.ActiveControl = null;
+            timer.Enabled = true;
+            Stop_button.Enabled = true;
+            Start_button.Enabled = false;
+        }
+
+        private void Stop_button_Click(object sender, EventArgs e)
+        {
+            Score = 0;
+            Score_TextBox.Text = "0";
+            timer.Enabled = false;
+            Complexity.Enabled = true;
+            Start_button.Enabled = true;
+            Stop_button.Enabled = false;
+        }
+    }
+}
